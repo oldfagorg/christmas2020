16a5671c595a8af55bd20b3f38ab7ef8f314db55
John200410 <43681932+John200410@users.noreply.github.com>
2020-12-25 00:44:54 -0500

    even more rare

--- src/main/java/org/oldfag/christmas/Listeners.java
+++ src/main/java/org/oldfag/christmas/Listeners.java
@@ -26,8 +26,8 @@ public class Listeners implements Listener {
 		//random object
 		final Random rand = new Random();
 		
-		//one in ten thousand chance (0.005%) that this will happen
-		final boolean spawn = rand.nextInt(20000) == 69; //we use 69 here because it is funny number
+		//one in ten thousand chance (0.002%) that this will happen
+		final boolean spawn = rand.nextInt(50000) == 69; //we use 69 here because it is funny number
 		
 		if(spawn && event.getWorld().getEnvironment().equals(World.Environment.NORMAL)) {
 			
